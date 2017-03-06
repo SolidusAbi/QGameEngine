8726584d8815202c133dec2fe4a9ecb0c24673df
Abián Hernández <ahguedes@aerolaser.es>
2017-03-06 17:01:24 +0000

    Creado el AnomalyGLWindow, utilizado para mostrar las anomalias de la aplicación AnomalyReports.

---
+++ AEOpenGLViewer/AnomalyGLWindow.cpp
@@ -0,0 +1,227 @@
+#include <math.h>
+#include <limits.h>
+
+#define _USE_MATH_DEFINES
+
+#include "Input.h"
+#include "AnomalyGLWindow.h"
+
+AnomalyGLWindow::AnomalyGLWindow() :AEOpenGLViewer(GLWindowViewConfig::Orthogonal, GLWindowMoveConfig::Freedom),
+anomaly_view(AnomalyView::none), anomaly_point(QVector3D(0, 0, 0)) {}
+
+AnomalyGLWindow::AnomalyGLWindow(int width, int height) : AEOpenGLViewer(GLWindowViewConfig::Orthogonal, GLWindowMoveConfig::Freedom, width, height),
+anomaly_view(AnomalyView::none), anomaly_point(QVector3D(0, 0, 0)) {}
+
+AnomalyGLWindow::AnomalyGLWindow(Vertex line_coord_0, Vertex line_coord_1) : AEOpenGLViewer(GLWindowViewConfig::Orthogonal, GLWindowMoveConfig::Freedom),
+anomaly_view(AnomalyView::none), anomaly_point(QVector3D(0, 0, 0)) {
+	setProjectionLine(line_coord_0, line_coord_1);
+}
+
+AnomalyGLWindow::AnomalyGLWindow(Vertex line_coord_0, Vertex line_coord_1, int window_width, int window_height) :
+	AEOpenGLViewer(GLWindowViewConfig::Orthogonal, GLWindowMoveConfig::Freedom, window_width, window_height) {
+	setProjectionLine(line_coord_0, line_coord_1);
+}
+
+AnomalyGLWindow::~AnomalyGLWindow() {}
+
+void AnomalyGLWindow::setAnomalyPoint(QVector3D anomaly_point) {
+	this->anomaly_point = anomaly_point;
+	reset(); // Centering camera in the anomaly point
+}
+
+void AnomalyGLWindow::setProjectionLine(Vertex coord_0, Vertex coord_1) {
+	if (projection_line_vertices.size() > 0)
+		projection_line_vertices.clear();
+
+	projection_line_vertices.push_back(coord_0);
+	projection_line_vertices.push_back(coord_1);
+
+	if (coord_1.getPosition().x() != coord_0.getPosition().x()) {
+		line_slope = (coord_1.getPosition().y() - coord_0.getPosition().y()) / (coord_1.getPosition().x() - coord_0.getPosition().x());
+		line_angle = atan(line_slope) * 180 / M_PI;
+	}
+	else {
+		line_slope = std::numeric_limits<float>::max(); //Inf
+		line_angle = 90;
+	}
+
+	//To draw line vertices  (Just for test)
+	//setVertices(std::vector<Vertex>{coord_0, coord_1}, GL_LINES);
+}
+
+int AnomalyGLWindow::topView(QVector3D anomaly_point) {
+	setAnomalyPoint(anomaly_point);
+	return topView();
+}
+
+int AnomalyGLWindow::topView() {
+	if (!lineDataPrepare())
+		return -1;
+
+	if (anomaly_view != AnomalyView::top)
+		reset();
+
+	//We will modify camera matrix, translate it to the anomaly point (anomaly point will be located in (0,0) coord)
+	anomaly_view = AnomalyView::top;
+
+	return 0;
+}
+
+int AnomalyGLWindow::crossView(QVector3D anomaly_point) {
+	setAnomalyPoint(anomaly_point);
+	return crossView();
+}
+
+int AnomalyGLWindow::crossView() {
+	if (!lineDataPrepare())
+		return -1;
+
+	if (anomaly_view != AnomalyView::cross)
+		reset();
+
+	anomaly_view = AnomalyView::cross;
+
+	camera.rotate(90, Camera3D::LocalRight);
+	camera.rotate(-(line_angle + 90), Camera3D::LocalForward);
+
+	update();
+
+	return 0;
+}
+
+int AnomalyGLWindow::longitudinalView(QVector3D anomaly_point) {
+	setAnomalyPoint(anomaly_point);
+	return longitudinalView();
+}
+
+int AnomalyGLWindow::longitudinalView() {
+	if (!lineDataPrepare())
+		return -1;
+
+	if (anomaly_view != AnomalyView::longitudinal)
+		reset();
+
+	anomaly_view = AnomalyView::longitudinal;
+
+	camera.rotate(90, Camera3D::LocalRight);
+	camera.rotate(-(line_angle + 180), Camera3D::LocalForward);
+
+	return 0;
+}
+
+void AnomalyGLWindow::reset() {
+	camera.restore();
+
+	//Center the view in the anomaly
+	camera.translate(camera.right(anomaly_point.x()));
+	camera.translate(camera.up(anomaly_point.y()));
+	camera.translate(-(camera.forward(anomaly_point.z())));
+
+	switch (anomaly_view) {
+	case AnomalyGLWindow::top:
+		topView();
+		break;
+	case AnomalyGLWindow::cross:
+		crossView();
+		break;
+	case AnomalyGLWindow::longitudinal:
+		longitudinalView();
+		break;
+	default:
+		AnomalyView::none;
+		break;
+	}
+}
+
+void AnomalyGLWindow::inputHandle(int key) {
+	// Camera Transformation
+	if (button_pressed) {
+		static const float transSpeed = 1.0f;
+
+		// Handle translations
+		QVector3D translation;
+		switch (key) {
+		case Qt::Key_W:
+			translation += camera.up();
+			break;
+		case Qt::Key_S:
+			translation -= camera.up();
+			break;
+		case Qt::Key_A:
+			translation -= camera.right();
+			break;
+		case Qt::Key_D:
+			translation += camera.right();
+			break;
+		case Qt::Key_R:
+			reset();
+			break;
+		case Qt::Key_C:
+			cleanGL();
+			break;
+		case Qt::Key_I:
+			toImage("C:/Users/ahguedes/Desktop/Prueba/prueba3.png");
+			break;
+		default:
+			break;
+		}
+		camera.translate(transSpeed * translation);
+	}
+}
+
+void AnomalyGLWindow::mouseMoveEvent(QMouseEvent * event) {
+	float ratio = width() / float(height());
+	QRect viewport(0, 0, this->width(), this->height());
+
+	QPoint mouseCurrPosition = event->pos();
+	QVector3D cursor_point = QVector3D(mouseCurrPosition.x(), height() - mouseCurrPosition.y(), 1);
+	QVector3D cursor_prev_point = QVector3D(mousePrevPosition.x(), height() - mousePrevPosition.y(), 1);
+
+	QVector3D cursor_delta2 = cursor_point - cursor_prev_point;
+
+	if (button_pressed) {
+		float steps_x = cursor_delta2.x() * ortho_width / width();
+		float steps_y = cursor_delta2.y() * ortho_width / height();
+
+		// Movement based in setOrthogonalProjection method.
+		if (width() <= height()) {
+			camera.translate(-camera.right(steps_x));
+			camera.translate(-camera.up(steps_y / ratio));
+		}
+		else {
+			camera.translate(-camera.right(steps_x * ratio));
+			camera.translate(-camera.up(steps_y));
+		}
+	}
+
+	update();
+	mousePrevPosition = mouseCurrPosition;
+}
+
+void AnomalyGLWindow::wheelEvent(QWheelEvent *event) {
+	static const float speed = 1.1f;
+
+	if (view_configuration == GLWindowViewConfig::Orthogonal) {
+		switch (event->modifiers()) {
+		case Qt::KeyboardModifier::NoModifier:
+			if (event->delta() > 0)
+				ortho_width /= speed;
+			else
+				ortho_width *= speed;
+			break;
+
+		case Qt::KeyboardModifier::ShiftModifier:
+			if (event->delta() > 0)
+				ortho_clipping /= speed;
+			else
+				ortho_clipping *= speed;
+			break;
+
+		default:
+			break;
+		}
+
+		setOrthogonalProjection(width(), height());
+		update();
+	}
+}
---
+++ AEOpenGLViewer/AnomalyGLWindow.h
@@ -0,0 +1,119 @@
+#pragma once
+#ifndef ANOMALYGLWINDOW_H
+#define ANOMALYGLWINDOW_H
+
+#include <QWheelEvent>
+#include <QMouseEvent>
+
+#include "Vertex.h"
+#include "AEOpenGLViewer.h"
+
+/**
+*	@class AnomalyGLWindow
+*	@brief QOpenGLWindow used in Anomaly Report. It is designed to use AEROLASER LIDAR data acquired from helicopter.
+*/
+class AnomalyGLWindow : public AEOpenGLViewer {
+
+	enum AnomalyView {
+		top,
+		cross,
+		longitudinal,
+		none
+	};
+
+public:
+	AnomalyGLWindow();
+	AnomalyGLWindow(int width, int height);
+	AnomalyGLWindow(Vertex line_coord_0, Vertex line_coord_1);
+	AnomalyGLWindow(Vertex line_coord_0, Vertex line_coord_1, int window_width, int window_height);
+	~AnomalyGLWindow();
+
+	void setProjectionLine(Vertex coord_0, Vertex coord_1);
+	std::vector<Vertex> getProjectionLineVertices();
+
+	template <typename T = float> void setAnomalyPoint(T x, T y, T z);
+	void setAnomalyPoint(QVector3D anomaly_point);
+	QVector3D anomalyPoint();
+
+	/**
+	*	@brief To generate a top view with the anomaly in the Window
+	*	@param x X coordinate of the anomaly
+	*	@param y Y coordinate of the anomaly
+	*	@param z Z coordinate of the anomaly
+	*
+	*	Anomaly Reports uses various types of views, this function is used in order to generate
+	*	the top view. There are more type of view implemented in the class (longitudinalView() and crossView()).
+	*	The TOP View is a default view in AEROLASER data, LAS data acquired from helicopter, this function only
+	*	moves the model in order to center it in the anomaly.
+	*/
+	template <typename T = float> int topView(T x, T y, T z);
+	int topView(QVector3D anomaly_point);
+	int topView();
+
+	/**
+	*	@brief To generate a cross view with the anomaly in the Window
+	*	@param x X coordinate of the anomaly
+	*	@param y Y coordinate of the anomaly
+	*	@param z Z coordinate of the anomaly
+	*
+	*	Anomaly Reports uses various types of views, this function is used in order to generate
+	*	the cross view. There are more type of view implemented in the class (longitudinalView() and topView()).
+	*/
+	template <typename T = float> int crossView(T x, T y, T z);
+	int crossView(QVector3D anomaly_point);
+	int crossView();
+
+	/**
+	*	@brief To generate a longitudinal view with the anomaly in the Window
+	*	@param x X coordinate of the anomaly
+	*	@param y Y coordinate of the anomaly
+	*	@param z Z coordinate of the anomaly
+	*
+	*	Anomaly Reports uses various types of views, this function is used in order to generate
+	*	the longitudinal view. There are more type of view implemented in the class (crossView() and topView()).
+	*/
+	template <typename T = float> int longitudinalView(T x, T y, T z);
+	int longitudinalView(QVector3D anomaly_point);
+	int longitudinalView();
+
+private:
+	//View information
+	AnomalyView anomaly_view;
+
+	// Line Information
+	bool lineDataPrepare();
+	std::vector<Vertex> projection_line_vertices;	//line coordinates are a XY point, Z-axis is irrelevant.
+	std::vector<QVector3D> line_points;	//line coordinates are a XY point, Z-axis is irrelevant.
+	float line_slope;
+	double line_angle;					//Inclination angle of the line with respect to the X-axis, in degrees
+
+										// Anomaly Information 
+	QVector3D anomaly_point;			//Anomaly point closest to the line
+
+										/**
+										*	@brief reseting the camera, camera will be centered in the anomaly point.
+										*/
+	void reset();
+
+	/**
+	*	@Brief Handle of the input command, this method must to be implemented
+	*		in each class that inherits from this one.
+	*/
+	void inputHandle(int key);
+protected:
+	void wheelEvent(QWheelEvent *event);
+	void mouseMoveEvent(QMouseEvent * event);
+};
+
+inline bool AnomalyGLWindow::lineDataPrepare() { return (projection_line_vertices.empty() ? false : true); }
+
+template <typename T> inline void AnomalyGLWindow::setAnomalyPoint(T x, T y, T z) { setAnomalyPoint(QVector3D(x, y, z)); }
+inline QVector3D AnomalyGLWindow::anomalyPoint() { return anomaly_point; }
+
+inline std::vector<Vertex> AnomalyGLWindow::getProjectionLineVertices() { return projection_line_vertices; }
+
+template <typename T> inline int AnomalyGLWindow::topView(T x, T y, T z) { return topView(QVector3D(x, y, z)); }
+template <typename T> inline int AnomalyGLWindow::crossView(T x, T y, T z) { return crossView(QVector3D(x, y, z)); }
+template <typename T> inline int AnomalyGLWindow::longitudinalView(T x, T y, T z) { return longitudinalView(QVector3D(x, y, z)); }
+
+#endif // ANOMALYGLWINDOW_H
